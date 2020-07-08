5ba4b068bdfe7e88c9b3ea43e0a236e325c40fb3
Daria <dashaost17@gmail.com>
2020-07-08 22:57:35 +0300

    Added level environment and character

---
+++ Assets/Scripts/CharacterControl/DamagerMovement.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class DamagerMovement : MonoBehaviour
+{
+    [SerializeField] private float _rotateSpeed;
+    private Transform _objTransform;
+
+    private void Start()
+    {
+        _objTransform = gameObject.GetComponent<Transform>();
+    }
+
+    private void Update()
+    {
+        _objTransform.Rotate(new Vector3(0f, 0f, -1f) * _rotateSpeed);
+    }
+}
---
+++ Assets/Scripts/CharacterControl/Mover.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+
+public class Mover : MonoBehaviour
+{
+    [SerializeField] private float _speed;
+    private float _velocityX;
+    private float _velocityY;
+
+    private bool _rightTurn = true;
+    private Rigidbody2D _rigidbody;
+
+    private void Start()
+    {
+        _rigidbody = GetComponent<Rigidbody2D>();
+
+    }
+
+
+    private void Update()
+    {
+        _velocityX = Input.GetAxisRaw("Horizontal");
+        _velocityY = _rigidbody.velocity.y;
+
+        _rigidbody.velocity = new Vector2(_velocityX * _speed, _velocityY);
+    }
+
+    private void LateUpdate()
+    {
+        var localScale = transform.localScale;
+        if (_velocityX > 0)
+        {
+            _rightTurn = true;
+ 
+        }
+        else if (_velocityX < 0)
+        {
+            _rightTurn = false;
+        }
+
+        if (((_rightTurn ) && (localScale.x < 0)) || ((!_rightTurn) && (localScale.x > 0)) )
+        {
+            localScale.x *= -1f;
+        }
+        transform.localScale = localScale;
+    }
+}
