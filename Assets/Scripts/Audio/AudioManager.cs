f730929c9b7b8147163c87f2b4b2ac4d3f73877c
Daria <dashaost17@gmail.com>
2020-07-28 14:52:57 +0300

    2D Lightning added

---
+++ Assets/Scripts/Audio/AudioManager.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+using UnityEngine.Audio;
+
+namespace Audio
+{
+    [System.Serializable]
+    public class AudioManager
+    {
+        [SerializeField] private AudioClip[] _sounds;
+        [SerializeField] private AudioClip _defaultClip;
+        [SerializeField] private AudioClip _gameMusic;
+        [SerializeField] private AudioMixer _mixer;
+
+        private AudioSource _sourceSFX;
+        private AudioSource _sourceMusic;
+        private float _musicVolume = 1f;                                     
+        private float _sfxVolume = 1f;
+
+        public AudioSource SourceMusic { get => _sourceMusic; private set => _sourceMusic = value; }
+        public AudioSource SourceSFX { get => _sourceSFX; set => _sourceSFX = value; }
+
+        public float MusicVolume
+        {
+            get
+            {
+                return _musicVolume;
+            }
+            set
+            {
+                _musicVolume = value;
+                SourceMusic.volume = _musicVolume;
+            }
+        }
+
+        public float SfxVolume
+        {
+            get
+            {
+                return _sfxVolume;
+            }
+            set
+            {
+                _sfxVolume = value;
+                SourceSFX.volume = _sfxVolume;
+            }
+        }
+
+        public void PlayMusic(bool playState)                                                    
+        {
+            if (playState)
+            {
+                SourceMusic.clip = _gameMusic;
+            }
+
+            SourceMusic.volume = MusicVolume;
+            SourceMusic.loop = true;
+            SourceMusic.Play();
+        }
+
+    }
+}
+
